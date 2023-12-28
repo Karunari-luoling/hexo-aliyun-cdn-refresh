7081d6a9aefc239b5bb46bbb4c6e46c78a530be3
Karunari <thunder_luoling@outlook.com>
2023-12-28 21:04:47 +0800

    第一次提交

---
+++ README.md
@@ -0,0 +1,24 @@
+# 安装
+```
+npm install hexo-aliyun-cdn-refresh
+```
+# 使用
+在 hexo 或主题的配置文件中添加如下内容即可启用插件：
+```
+hacr:
+  # 是否启用插件
+  enable: true
+  # 是否在发布后自动执行脚本
+  auto_push: true
+  # 网站地址
+  site: https://byer.top/
+  Access:
+  # 阿里云CDN Access Token
+    accessKeyId: L##########################2
+    accessKeySecret: 9######################EH
+  RefreshOpts:
+    # 刷新类型 refresh刷新 push预热 both刷新cdn后预热
+    type: refresh
+    # 刷新目录directory 刷新文件file并在_data中创建refresh-list.json文件内容为["https://byer.top/1.png","https://byer.top/2.png"]
+    method: file
+```
---
+++ index.js
@@ -0,0 +1,83 @@
+const logger = require('hexo-log').default();
+const AliyunCdnService = require('./lib/api');
+var fs = require('hexo-fs');
+var path = require('path');
+
+function start(hexo) {
+    const config = hexo.config;
+    const pluginConfig = config['hacr'] ?? config.theme_config['hacr'];
+    if (!pluginConfig?.enable)
+        return;
+    if (pluginConfig['auto_exec']) {
+        hexo.on('deployAfter', async () => {
+            await runHacr(hexo, pluginConfig);
+        });
+    }
+    else {
+        hexo.extend.console.register('hacr', '刷新阿里云CDN', {}, async () => {
+            await runHacr(hexo, pluginConfig);
+        });
+    }
+}
+
+async function runHacr(hexo, pluginConfig) {
+    const config = pluginConfig;
+    const accessKeyId = config.Access.accessKeyId
+    const accessKeySecret = config.Access.accessKeySecret
+    const site = config.site
+    const type = config.RefreshOpts.type
+    const area = undefined
+    let method = config.RefreshOpts.method
+    method = method.charAt(0).toUpperCase() + method.slice(1).toLowerCase();
+
+    if(!accessKeyId || !accessKeySecret) {
+        logger.warn('<AccessKey> <AccessKeySecret> 参数不能为空');
+        return;
+    }
+    if(!site) {
+        logger.warn('<site> 参数不能为空');
+        return;
+    }
+
+    logger.info('开始刷新阿里云CDN');
+    const access = { accessKeyId: accessKeyId, accessKeySecret: accessKeySecret };
+    const cdnService = new AliyunCdnService(access);
+    
+    const actions = {
+        push: opts => cdnService.push(opts),
+        refresh: opts => cdnService.refresh(opts),
+        both: async opts => {
+            await cdnService.refresh(opts);
+            await cdnService.push(opts);
+        }
+    };
+    
+    if (method === 'File') {
+        const filePath = path.join(process.cwd(), '/source/_data/refresh-list.json');
+        if (fs.existsSync(filePath)) {
+            const data = require(filePath);
+            for (const item of data) {
+                let opts = { objectPath: item, objectType: method, area: area };
+                if (actions[type]) {
+                    await actions[type](opts);
+                }
+            }
+        } else {
+            return logger.warn('没有找到refresh-list.json，请在对应位置创建文件');
+        }
+    }
+    
+    if (method === 'Directory') {
+        let opts = { objectPath: site, objectType: method, area: area };
+        if (actions[type]) {
+            await actions[type](opts);
+        }
+    }
+}
+
+try {
+    start(hexo);
+}
+catch (e) {
+    logger.error(e);
+}
